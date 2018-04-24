963003bdc5f27a7c2565fc0a3a77d05a24fe5bd6
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-04-23 17:42:36 -0700

    Added 404 error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,18 @@
 '''
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
+@app.errorhandler(404)
+def not_found(error):
+    '''
+        Handles page not found for API
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.teardown_appcontext
 def close_storage(exception):
