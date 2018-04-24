88eacb09d0c1423cd60f1276c3c43f18ad8707cb
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-04-23 17:24:53 -0700

    Create an endpoint that retriees the number of each object by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,22 @@
 '''
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage, classes
 
 @app_views.route('/status')
 def status():
     '''
+    Returns a STATUS OK when pinged
     '''
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def count_objects():
+    '''
+    Returns the jsonified version of each counted object
+    '''
+    count_dict = {}
+    for key, value in classes.items():
+        if key != "BaseModel":
+            count_dict[key.lower()] = storage.count(key)
+    return jsonify(count_dict)
