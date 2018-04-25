a49773cf34e87a52304e455685b983eb68cec963
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-04-25 14:41:55 -0700

    Converts port variable to integer

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 import os
 
 host = os.getenv('HBNB_API_HOST', default="0.0.0.0")
-port = os.getenv('HBNB_API_PORT', default=5000)
+port = int(os.getenv('HBNB_API_PORT', default=5000))
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
