8f2c67a5023f5d6d9de6fae3c6be48a64936c1ee
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-04-26 22:49:56 -0700

    Fix typos

--- tests/test_api/test_v1/test_views/test_states.py
+++ tests/test_api/test_v1/test_views/test_states.py
@@ -55,7 +55,7 @@ class Test_States(unittest.TestCase):
         response = self.test_app.get('/api/v1/states/{}'.format(state1.id))
         check = json.loads(response.data.decode(defenc()))
         check_state = check['id']
-        self.assertEqual(state1.id, check_state, msg=check)
+        self.assertEqual(state1.id, check_state)
 
     def test_states_invalid_id(self):
         '''
@@ -72,7 +72,7 @@ class Test_States(unittest.TestCase):
         state1.save()
         response = self.test_app.delete('/api/v1/states/{}'.format(state1.id))
         check = json.loads(response.data.decode(defenc()))
-        self.assertEqual(storage.get("State", state1.id, ), None)
+        self.assertEqual(storage.get("State", state1.id), None)
 
     def test_delete_invalid_ID(self):
         '''
