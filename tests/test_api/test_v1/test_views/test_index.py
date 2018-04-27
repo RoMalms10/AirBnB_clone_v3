dc617deb6fbf912afe6f5cea9b79aa2e8408f023
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-04-26 19:29:56 -0700

    Added two tests for index

--- tests/test_api/test_v1/test_views/test_index.py
+++ tests/test_api/test_v1/test_views/test_index.py
@@ -7,6 +7,7 @@ import sys
 import json
 from models import storage
 from api.v1.app import app
+from sys import getdefaultencoding as defenc
 
 
 class Test_Index(unittest.TestCase):
@@ -19,3 +20,30 @@ class Test_Index(unittest.TestCase):
             Starts up flask for testing
         '''
         self.test_app = app.test_client()
+
+    def test_app_status(self):
+        """
+        Tests /status endpoint
+        """
+        response = self.test_app.get('/api/v1/status')
+        self.assertEqual(json.loads(
+            response.get_data().decode(defenc())),
+            {'status': 'OK'}
+        )
+
+    def test_app_stats(self):
+        """
+        Tests /stats endpoint
+        """
+        response = self.test_app.get('/api/v1/stats')
+        self.assertEqual(json.loads(
+            response.get_data().decode(defenc())),
+            {
+                "amenities": storage.count("Amenity"),
+                "cities": storage.count("City"),
+                "places": storage.count("Place"),
+                "reviews": storage.count("Review"),
+                "states": storage.count("State"),
+                "users": storage.count("User")
+            }
+        )
