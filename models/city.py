eff0effe85435b228d3fb704c4df35ee20fbaf7d
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-04-26 22:52:45 -0700

    Added argument so unittests don't complain about incorrect deletion count

--- models/city.py
+++ models/city.py
@@ -32,6 +32,11 @@ class City(BaseModel, Base):
     '''
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "cities"
+
+        __mapper_args__ = {
+        'confirm_deleted_rows': False
+        }
+
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship("Place", backref="cities",
