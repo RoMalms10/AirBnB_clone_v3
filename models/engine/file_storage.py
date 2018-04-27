b530d73165544ec5c63958be6121e8beca867347
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-04-26 22:18:35 -0700

    Update Documentation for Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-    Define class FileStorage
+    Implementation of the FileStorage class.
 '''
 import json
 import models
@@ -8,14 +8,35 @@ import models
 
 class FileStorage:
     '''
-        Serializes instances to JSON file and deserializes to JSON file.
+    FileStorage Class.
+    A type of storage method that uses JSON and files to store data.
+    When the environment variable HBNB_TYPE_STORAGE is not equal to db, this
+        storage method will be used.
+     Methods:
+        all
+        new
+        save
+        delete
+        reload
+        close
+        get
+        count
     '''
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
         '''
-            Return the dictionary
+        Loads all objects in storage and returns a dictionary.
+        This method can take in a class or not. If no class is passed, returns
+            all objects in the database. If a class is passed, returns
+            all objects of that type.
+        Arguments:
+            cls: Class passed, defaults to None.
+        Returns:
+            Dictionary representation of the query in the form of:
+                key: str(object).id
+                value: the actual object with that id
         '''
         if cls is None:
             return self.__objects
@@ -29,9 +50,10 @@ class FileStorage:
 
     def new(self, obj):
         '''
-            Set in __objects the obj with key <obj class name>.id
-            Aguments:
-                obj : An instance object.
+        Adds the object to the current storage session by adding a key and
+            value to the self.__objects dictionary.
+        Arguments:
+            obj: object passed
         '''
         key = str(obj.__class__.__name__) + "." + str(obj.id)
         value_dict = obj
@@ -39,7 +61,9 @@ class FileStorage:
 
     def save(self):
         '''
-            Serializes __objects attribute to JSON file.
+        Saves all changes of the current session by committing the changes
+            to the current storage session by serializes all the values in 
+            self.__objects to the JSON file.
         '''
         objects_dict = {}
         for key, val in self.__objects.items():
@@ -50,7 +74,8 @@ class FileStorage:
 
     def reload(self):
         '''
-            Deserializes the JSON file to __objects.
+        Deserializes the JSON file to self.__objects by populating key, value
+            pairs and instanciating the objects in the values.
         '''
         try:
             with open(self.__file_path, encoding="UTF8") as fd:
@@ -64,7 +89,8 @@ class FileStorage:
 
     def delete(self, obj=None):
         '''
-        Deletes an object from __objects if it is inside of __objects
+        Deletes the object from storage by deleting it in self.__objects and
+            saving the dictionary back into the JSON file.
         '''
         copy_storage = dict(self.__objects)
         desired_key = obj
@@ -76,7 +102,7 @@ class FileStorage:
 
     def close(self):
         '''
-        Method calls reload method to deserialize JSON file to objects
+        Method calls reload method to deserialize JSON file to objects.
         '''
         self.reload()
 
@@ -84,9 +110,14 @@ class FileStorage:
         '''
         Method that retrieves an object based off the class (cls) passed
         and the id (id) passed
-        Attributes:
+        Arguments:
             cls: string representing the class name
             id: string representing the object ID
+        Attributes:
+            cls_dict: the dictionary received from the all method when
+                passing a class.
+        Returns:
+            the object if found, or nothing
         '''
         cls_dict = self.all(cls)
 
@@ -99,10 +130,17 @@ class FileStorage:
             return None
 
     def count(self, cls=None):
-        '''
+         '''
         Method that counts how many objects of the type cls being passed.
-        Attributes:
+        Arguments:
             cls: string representing the class name (optional)
+        Attributes:
+            all_dict: the dictionary received from the all method when no
+                class is passed.
+            cls_dict: the dictionary received from the all method when
+                pasing a class.
+        Returns:
+            The count of objects
         '''
         cls_dict = {}
         all_dict = {}
