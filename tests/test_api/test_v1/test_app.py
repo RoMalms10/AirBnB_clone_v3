6ed61f36b410b154786e9f8e7f839364bfb77cff
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-04-26 17:55:06 -0700

    Create test for app.py method

---
+++ tests/test_api/test_v1/test_app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Unit tests for app.py
+"""
+from models import storage
+from api.v1.app import app
+import json
+import sys
+import unittest
+
+
+class Test_app_py(unittest.TestCase):
+    """
+    Tests the app.py file to see if the methods and endpoints are working
+    properly.
+    """
+    def setUp(self):
+        """
+        Sets up the app for testing
+        """
+        self.test_app = app.test_client()
+
+    def test_app_error_handler_404(self):
+        """
+        Tests the errorhandler(404) method
+        """
+        response = self.test_app.get('/api/v1/views/error')
+        self.assertEqual(json.loads(
+            response.get_data().decode(sys.getdefaultencoding())), {'error': 'Not found'})
